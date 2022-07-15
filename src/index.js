7e2d6250c61e43812cde6d1edf9993f319f53738
anmoldeep1512 <anmoldeep1512@gmail.com>
2022-07-15 22:24:49 +0530

    changed favicon and app name

--- src/index.js
+++ src/index.js
@@ -4,4 +4,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(
+  document.getElementById('root'))
+  .render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
