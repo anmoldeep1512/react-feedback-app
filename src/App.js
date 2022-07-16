ac776e605f2f08f4665416325864a87ffc4ff6cd
anmoldeep1512 <anmoldeep1512@gmail.com>
2022-07-16 22:51:25 +0530

    created feedback list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Header from "./components/Header";
+
 
 function App() {
   return (
+    <>
     <h1>App</h1>
+
+    <Header />
+
+    </>
   );
 }
 
---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function FeedbackItem() {
+  return (
+    <Card>
+      
+      <div className='num-display'>{item.rating}</div>
+      <button onClick={() => deleteFeedback(item.id)} className='close'>
+        <FaTimes color='purple' />
+      </button>
+      <button onClick={() => editFeedback(item)} className='edit'>
+        <FaEdit color='purple' />
+      </button>
+      <div className='text-display'>{item.text}</div>
+    </Card>  )
+}
+
+export default FeedbackItem
