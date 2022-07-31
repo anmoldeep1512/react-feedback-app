e62d154108c362aa23e7c40b8afc461ef40089a2
anmoldeep1512 <anmoldeep1512@gmail.com>
2022-08-01 03:56:19 +0530

    final commit

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { FaTimes, FaEdit } from 'react-icons/fa'
+import { useContext } from 'react'
+import PropTypes from 'prop-types'
+import Card from './shared/Card'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackItem() {
+function FeedbackItem({ item }) {
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext)
+
   return (
     <Card>
-      
       <div className='num-display'>{item.rating}</div>
       <button onClick={() => deleteFeedback(item.id)} className='close'>
         <FaTimes color='purple' />
@@ -12,7 +17,12 @@ function FeedbackItem() {
         <FaEdit color='purple' />
       </button>
       <div className='text-display'>{item.text}</div>
-    </Card>  )
+    </Card>
+  )
+}
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
 }
 
 export default FeedbackItem
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,7 @@
-import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+import PropTypes from "prop-types"
+import { Link } from "react-router-dom"
 
 function Header({ text, bgColor, textColor }) {
-
   const headerStyles = {
     backgroundColor: bgColor,
     color: textColor,
@@ -11,25 +10,24 @@ function Header({ text, bgColor, textColor }) {
   return (
     <header style={headerStyles}>
       <div className='container'>
+        <Link to='/' style={{ textDecoration: "none", color: "#ff6a95" }}>
           <h2>{text}</h2>
-          
-      
+        </Link>
       </div>
     </header>
   )
 }
 
 Header.defaultProps = {
-    text: "Feedback UI",
-    bgColor: "rgba(0,0,0,0.4)",
-    textColor: "#ff6a95",
-  }
-  
-  Header.propTypes = {
-    text: PropTypes.string,
-    bgColor: PropTypes.string,
-    textColor: PropTypes.string,
-  }
+  text: "Feedback UI",
+  bgColor: "rgba(0,0,0,0.4)",
+  textColor: "#ff6a95",
+}
 
+Header.propTypes = {
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+}
 
 export default Header
